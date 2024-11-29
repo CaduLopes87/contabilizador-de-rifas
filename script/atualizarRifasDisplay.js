8c3f364708ea251acc4a32a6a9d57055726c1957
CaduLopes87 <edu.lopesbezerra@gmail.com>
2024-11-29 20:00:33 -0300

    UPDATE: Atualiza criação dos elementos visuais

--- script/atualizarRifasDisplay.js
+++ script/atualizarRifasDisplay.js
@@ -7,33 +7,35 @@ export function atualizarRifasDisplay(){
     let listaRifas = [];
 
     bancoDeDadosRifas.listar().then(rifas => {
-       listaRifas = rifas;
-
+       const fragment = document.createDocumentFragment();
+       
+       rifas.forEach(rifa => { 
+           const pNome = document.createElement('p');
+           const pNumero = document.createElement('p');
+           const pPagamento = document.createElement('p');
+           const botaoExcluirElemento = document.createElement('button');
+           const divRifaDados = document.createElement('div');
+           
+           divRifaDados.classList.add("rifa-dados");
+           divRifaDados.setAttribute('data-rifa-id', `rifa-${rifa.id}`);
+           
+           botaoExcluirElemento.classList.add('excluir-rifa');
+           botaoExcluirElemento.classList.add('hidden');
+           botaoExcluirElemento.setAttribute('data-excluir-bt-id',  `${rifa.id}`);
+   
+           botaoExcluirElemento.innerText = 'X';
+           pNome.innerText = `Nome: ${rifa.nome}`;
+           pNumero.innerText = `Número: ${rifa.numero}`;
+           pPagamento.innerText = `Pagamento: ${rifa.pagamento}`;
+           
+           divRifaDados.appendChild(botaoExcluirElemento);
+           divRifaDados.appendChild(pNome);
+           divRifaDados.appendChild(pNumero);
+           divRifaDados.appendChild(pPagamento);
+           fragment.appendChild(divRifaDados);            
+       });
+       
+       rifasContainer.appendChild(fragment);
     });
-    
-    listaRifas.forEach(rifa => { 
-        const pNome = document.createElement('p');
-        const pNumero = document.createElement('p');
-        const pPagamento = document.createElement('p');
-        const botaoExcluirElemento = document.createElement('button');
-        const divRifaDados = document.createElement('div');
-        
-        divRifaDados.classList.add("rifa-dados");
-        divRifaDados.setAttribute('data-rifa-id', `rifa-${rifa.id}`);
-        
-        botaoExcluirElemento.classList.add('excluir-rifa');
-        botaoExcluirElemento.classList.add('hidden');
-        botaoExcluirElemento.setAttribute('data-excluir-bt-id',  `${rifa.id}`);
 
-        botaoExcluirElemento.innerText = 'X';
-        pNome.innerText = `Nome: ${rifa.nome}`;
-        pNumero.innerText = `Número: ${rifa.numero}`;
-        pPagamento.innerText = `Pagamento: ${rifa.pagamento}`;
-        
-        divRifaDados.appendChild(botaoExcluirElemento);
-        divRifaDados.appendChild(pNome);
-        divRifaDados.appendChild(pNumero);
-        divRifaDados.appendChild(pPagamento);            
-        rifasContainer.appendChild(divRifaDados);
-    });
 }
